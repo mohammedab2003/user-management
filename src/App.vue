b91893926cd27d6f17aee2e3fddad063773e0d06
mohammedab2003 <152696041+mohammedab2003@users.noreply.github.com>
2024-07-06 19:23:04 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,5 @@
 <script setup>
-import { onBeforeMount } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import { auth } from "./firebase/db";
 
-const router = useRouter();
-const route = useRoute();
-
-onBeforeMount(() => {
-  auth.onAuthStateChanged((user) => {
-    if (!user) {
-      router.replace("/login");
-    } else if (route.path == "/login" || route.path == "/register") {
-      router.replace("/");
-    }
-  });
-});
 </script>
 
 <template>
